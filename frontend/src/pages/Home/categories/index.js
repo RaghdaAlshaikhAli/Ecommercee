0093aa77acd94018e6db007f00404b3c64a17dfd
unknown <raghdaayman>
2023-06-10 14:15:32 +0300

    add edits

--- frontend/src/pages/Home/categories/index.js
+++ frontend/src/pages/Home/categories/index.js
@@ -5,7 +5,6 @@ import './style.scss'
 import OneCategory from './oneCategroy'
 import fur from '../../../assets/images/fur.jpg'
 import bag from '../../../assets/images/bag.jpg'
-import labtop from '../../../assets/images/labtop.jpg'
 import shoes from '../../../assets/images/shoes.jpg'
 import travel from '../../../assets/images/travel.jpg'
 import book from '../../../assets/images/book.jpg'
@@ -15,12 +14,11 @@ const Category = () => {
         <div className='category'>
             <h3>Shop Our Top Categories</h3>
             <div className='flex'>
-                <OneCategory title={'Furniture'} photo={fur} />
-                <OneCategory title={'Hand Bag'} photo={bag} />
-                <OneCategory title={'Books'} photo={book} />
-                <OneCategory title={'Tech'} photo={labtop} />
-                <OneCategory title={'Sneakers'} photo={shoes} />
-                <OneCategory title={'Travel'} photo={travel} />
+                <OneCategory title={'Burger'} photo={fur} />
+                <OneCategory title={'Pizza'} photo={bag} />
+                <OneCategory title={'staek'} photo={book} />
+                <OneCategory title={'beef stroganof'} photo={shoes} />
+                <OneCategory title={'Kentucky'} photo={travel} />
             </div>
         </div>
     )
